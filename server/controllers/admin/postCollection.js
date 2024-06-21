715cebba0b7920a118b40b4dde2a82e30f9bd346
dinonomous <dineshwear@gmail.com>
2024-06-21 13:53:04 +0530

    Feature : Createing Subject from Admin Post

---
+++ server/controllers/admin/postCollection.js
@@ -0,0 +1,25 @@
+const unitSchema = require("../../models/units");
+const { Subject, QuizDB } = require('../../config/db');
+
+const postCollectionTitles = async (req, res) => {
+    try {
+        const collectionName = req.params.collection;
+        const body = req.body;
+        const Unitc = Subject.model(collectionName, unitSchema, collectionName);
+    
+        const createdSub = await Unitc.create(body);
+    
+        const documents = await Unitc.find({ title: { $exists: true } });
+    
+        // Respond with the created document and other documents with titles
+        res.json({ created: createdSub, allDocuments: documents });
+    } catch (error) {
+      console.error("Error creating collection:", error);
+      res.status(500).json({ error: "Failed to retrieve collection titles", errorMessage: error.message });
+    }
+  };
+
+
+module.exports={
+    postCollectionTitles
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const { getCollectionNames, getCollectionNamesQuiz } = require('../controllers/getCollection')
 const { getCollectionTitles, getQuizCollectionTitles } = require('../controllers/getCollectionTitles')
+const { postCollectionTitles } = require('../controllers/admin/postCollection')
 const { getUnitDetails, getQuizDetails } = require('../controllers/getInfo')
 const { Subject, QuizDB } = require('../config/db');
 
 router.get("/getcollectionnames", getCollectionNames );
 
 router.get("/:collection", getCollectionTitles);
+
+router.post("/api/admin/subject/:collection", postCollectionTitles);
 
 router.get("/api/quizapi/Quizz/:collection", getQuizCollectionTitles);
 
