f99c731305d7a5cbda69da830d6147f86e90d703
dinonomous <dineshwear@gmail.com>
2024-07-02 12:13:16 +0530

    Configure: CORS

--- server/app.js
+++ server/app.js
@@ -3,16 +3,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
-require('./config/passport')
+require('./config/passport');
 require('dotenv').config();
 
 const apiUrl = process.env.API_URL;
 const apiFrontUrl = process.env.FRONTEND_API_URL;
-app.use(cors({
-  origin: `${apiFrontUrl}`,
-  credentials: true
-}));
-
 
 var indexRouter = require('./routes/index');
 var authentication = require('./routes/authentication');
@@ -20,14 +15,22 @@ const passport = require('passport');
 
 var app = express();
 
+// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
+
+// CORS setup
+app.use(cors({
+  origin: [apiFrontUrl, 'http://another-allowed-origin.com'],
+  credentials: true
+}));
+
+// Routes setup
 app.use('/', indexRouter);
 app.use('/authentication', authentication);
-
 
 module.exports = app;
