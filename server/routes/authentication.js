d6e8762fd1d880f45a80244297ca9ffdc3d4fbd0
Dineshwar <133356394+dinonomous@users.noreply.github.com>
2024-09-14 14:49:14 +0530

    changes

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -5,108 +5,104 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 const { secretOrKey } = require('../config/keys');
 
-router.get('/',(req,res)=>{
-    res.send("welcome to auth")
-})
+router.get('/', (req, res) => {
+  res.send("welcome to auth");
+});
 
 router.options("/login", (req, res) => {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.sendStatus(200);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(200);
+});
+
+router.post('/login', async (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  UserModel.findOne({ email: req.body.email }).then((user) => {
+    if (!user) {
+      return res.status(401).send({
+        success: false,
+        message: "No user found",
+      });
+    }
+
+    if (!compareSync(req.body.password, user.password)) {
+      return res.status(401).send({
+        success: false,
+        message: "Wrong password, try again",
+      });
+    }
+
+    const payload = {
+      email: user.email,
+      id: user._id,
+      userType: "internal",
+    };
+
+    const token = jwt.sign(payload, `${secretOrKey}`, { expiresIn: "1d" });
+
+    // Cookie configuration based on environment
+    if (process.env.NODE_ENV === 'development') {
+      res.setHeader('Set-Cookie', [`token=${token}; Path=/; HttpOnly; Max-Age=86400000; SameSite=Lax;`]);
+    } else {
+      res.setHeader('Set-Cookie', [`token=${token}; Path=/; HttpOnly; Max-Age=86400000; SameSite=None; Secure=true;`]);
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: "Logged in!",
+      email: user.email,
+    });
   });
-
-router.post('/login',async(req,res)=>{
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Credentials", "true");
-
-        UserModel.findOne({ email: req.body.email }).then((user) => {
-          if (!user) {
-            return res.status(401).send({
-              success: false,
-              message: "no user found",
-            });
-          }
-    
-          if (!compareSync(req.body.password, user.password)) {
-            return res.status(401).send({
-              success: false,
-              message: "wrong password try again",
-            });
-          }
-    
-          const payload = {
-            email: user.email,
-            id: user._id,
-            userType: "internal",
-          };
-    
-          const token = jwt.sign(payload, `${secretOrKey}`, { expiresIn: "1d" });
-          
-          res.cookie("token", token, {
-            path: "/",  // Available across the entire app
-            domain: "srm-launch-pad-api.vercel.app",  // Your backend's domain
-            httpOnly: false,  // For security, set this to true for non-JS access
-            secure: true,  // Set to true if you're using HTTPS
-            sameSite: "strict"
-          });          
-          
-
-          return res.status(200).send({
-            success: true,
-            message: "logged in!",
-
-            email: user.email,
-          });
-        });
-})
+});
 
 router.options('/register', (req, res) => {
-    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_API_URL);
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.sendStatus(200);
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_API_URL);
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
+
+router.post('/register', (req, res) => {
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_API_URL);
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  let user = new UserModel({
+    email: req.body.email,
+    password: hashSync(req.body.password, 10),
   });
 
-router.post('/register',(req,res)=>{
-    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_API_URL);
-    res.header('Access-Control-Allow-Credentials', 'true');
-    let user = new UserModel({
-        email: req.body.email,
-        password: hashSync(req.body.password,10)
-    })
-
-    user.save().then(user => {
-        res.send({
-            success: true,
-            message: "user created",
-            user:{
-                id: user._id,
-                username: user.username
-            }
-
-        })
-    }).catch(err => {
-        res.send({
-            success: false,
-            message: "not created",
-            error: err
-
-        })
-    })
+  user.save().then(user => {
+    res.send({
+      success: true,
+      message: "User created",
+      user: {
+        id: user._id,
+        username: user.username,
+      },
+    });
+  }).catch(err => {
+    res.send({
+      success: false,
+      message: "User not created",
+      error: err,
+    });
+  });
 });
 
 router.post('/check-email', async (req, res) => {
-    const { email } = req.body;
-    const user = await UserModel.findOne({ email });
-  
-    if (user) {
-      return res.status(200).json({ exists: true });
-    } else {
-      return res.status(200).json({ exists: false });
-    }
-  });
+  const { email } = req.body;
+  const user = await UserModel.findOne({ email });
+
+  if (user) {
+    return res.status(200).json({ exists: true });
+  } else {
+    return res.status(200).json({ exists: false });
+  }
+});
 
 module.exports = router;
