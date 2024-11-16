e6360832a6dbca41ec166abda4ad8d4b00888501
dinonomous <dineshwear@gmail.com>
2024-11-16 09:25:27 +0530

    fix: auth routes

--- server/routes/googleRouter.js
+++ server/routes/googleRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { listFolderContents, listRootFolders } = require('../controllers/driveController');
+const passport = require("passport");
+require("../config/passport");
 
 const router = express.Router();
 
-router.get('/files/:folderId', listFolderContents);
-
-router.get('/files', listRootFolders);
+router.get('/files/:folderId', passport.authenticate("jwt", { session: false }), listFolderContents);
+ 
+router.get('/files', passport.authenticate("jwt", { session: false }), listRootFolders);
 
 module.exports = router;
