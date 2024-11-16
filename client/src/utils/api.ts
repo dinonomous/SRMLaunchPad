3bb2ac8dad1053f21c26d0981a503e46ce73ed16
dinonomous <dineshwear@gmail.com>
2024-11-16 09:38:11 +0530

    fix: credential headers

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,35 +1,38 @@
-// src/utils/api/v2/v2.ts
 import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-// Function to fetch all collections
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
 export const getCollectionNames = async () => {
-  const response = await axios.get(`${API_URL}/getcollectionnames`);
+  const response = await axiosInstance.get(`/getcollectionnames`);
   return response.data;
 };
 
 export const getProgramsData = async (subject: string, id: string) => {
-  const response = await axios.get(`${API_URL}/subjects/${subject}/${id}`);
+  const response = await axiosInstance.get(`/subjects/${subject}/${id}`);
   return response.data;
 };
 
 export const getBooksRoot = async () => {
-  const response = await axios.get(`${API_URL}/google/files?pageSize=10`);
+  const response = await axiosInstance.get(`/google/files?pageSize=10`);
   return response.data;
 };
 
 export const getBooksRootExpand = async (id: string) => {
-  const response = await axios.get(`${API_URL}/google/files?pageSize=10&pageToken=${id}`);
+  const response = await axiosInstance.get(`/google/files?pageSize=10&pageToken=${id}`);
   return response.data;
 };
 
 export const getBooksChild = async (id: string) => {
-  const response = await axios.get(`${API_URL}/google/files/${id}?pageSize=10`);
+  const response = await axiosInstance.get(`/google/files/${id}?pageSize=10`);
   return response.data;
 };
 
 export const getBooksChildExpand = async (id: string, next: string) => {
-  const response = await axios.get(`${API_URL}/google/files/${id}?pageSize=10&pageToken=${next}`);
+  const response = await axiosInstance.get(`/google/files/${id}?pageSize=10&pageToken=${next}`);
   return response.data;
 };
