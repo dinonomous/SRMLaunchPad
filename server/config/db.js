be949ad034e9eb81a356675ca640e65e68a028f2
dinonomous <dineshwear@gmail.com>
2024-11-17 21:41:30 +0530

    switching to old one

--- server/config/db.js
+++ server/config/db.js
@@ -1,71 +1,18 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+require('dotenv').config();
 
-/**
- * MongoDB Cluster Connection String from Environment Variables
- * @constant {string}
- */
-const MONGODB_CLUSTER_CONNECTION_STRING =
-  process.env.MONGODB_CLUSTER_CONNECTION_STRING;
+const subjectConnectionString = process.env.MONGODB_SUBJECT_CONNECTION_STRING;
+const quizDBConnectionString = process.env.MONGODB_QUIZDB_CONNECTION_STRING;
+const trashDBConnectionString = process.env.MONGODB_TRASHDB_CONNECTION_STRING;
 
-if (!MONGODB_CLUSTER_CONNECTION_STRING) {
-  console.error("Error: Missing MongoDB cluster connection string.");
-  process.exit(1);
-}
+const LearningModule = mongoose.createConnection(subjectConnectionString, { autoCreate: false });
+const tests = mongoose.createConnection(quizDBConnectionString, { autoCreate: false });
+const TrashDB = mongoose.createConnection(trashDBConnectionString, { autoCreate: false });
+const user = mongoose.createConnection(process.env.MONGODB_USERDB_CONNECTION_STRING, { autoCreate: false });
 
-/**
- * MongoDB Connection Options
- * @constant {object}
- */
-const mongooseOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-/**
- * Separate MongoDB Connections for Different Databases
- */
-const LearningModule = mongoose.createConnection(
-  `${MONGODB_CLUSTER_CONNECTION_STRING}/SRMLaunchpad2`,
-  mongooseOptions
-);
-
-LearningModule.on("connected", () =>
-  console.log("✅ Connected to LearningModule database")
-);
-LearningModule.on("error", (error) =>
-  console.error("❌ Error in saperae database connection:", error)
-);
-
-/**
- * Additional Connections (if required)
- * Example for another database:
- */
-const tests = mongoose.createConnection(
-  `${MONGODB_CLUSTER_CONNECTION_STRING}/Quizz`,
-  mongooseOptions
-);
-
-tests.on("connected", () => console.log("✅ Connected to tests database"));
-tests.on("error", (error) =>
-  console.error("❌ Error in saperae database connection:", error)
-);
-
-const user = mongoose.createConnection(
-  `${MONGODB_CLUSTER_CONNECTION_STRING}/Users`,
-  mongooseOptions
-);
-
-user.on("connected", () => console.log("✅ Connected to Users database"));
-user.on("error", (error) =>
-  console.error("❌ Error in saperae database connection:", error)
-);
-
-/**
- * Exports
- */
 module.exports = {
   LearningModule,
+  TrashDB,
   tests,
-  user,
+  user
 };
